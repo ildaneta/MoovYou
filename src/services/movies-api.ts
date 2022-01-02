b40580ba2a32d523ae1d7752f07ca089b54495e0
ildaneta <iildaemanoely@gmail.com>
2022-01-02 09:47:53 -0300

    refactor: adding new methods to call the api and apiImage

--- src/services/movies-api.ts
+++ src/services/movies-api.ts
@@ -1,9 +1,32 @@
-import api from './api';
+import {api, apiImage} from './api';
+
+interface MoviesAPIProps {
+  api_key: string;
+  page_number: number;
+  movie_id: number;
+  image_url: string
+}
 
 const MoviesAPI = {
-  getMoviesListNowPlaying: async (api_key: string, page_number: number) => {
+  getMoviesListNowPlaying: async ({api_key, page_number}: MoviesAPIProps) => {
     return (await api).get(`movie/now_playing?api_key=${api_key}=en-US&page=${page_number}`)
-  }
+  },
+
+  getMoviesGenreList: async ({api_key}: MoviesAPIProps) => {
+    return (await api).get(`genre/movie/list?api_key=${api_key}&language=en-US`)
+  },
+
+  getMovieDetails: async ({api_key, movie_id}: MoviesAPIProps) => {
+    return (await api).get(`movie/${movie_id}?api_key=${api_key}&language=en-US`)
+  },
+
+  getMovieCast: async ({api_key, movie_id}: MoviesAPIProps) => {
+    return (await api).get(`movie/${movie_id}/credits?api_key=${api_key}&language=en-US`)
+  },
+
+  getImage: async ({image_url}: MoviesAPIProps) => {
+    return (await apiImage).get(`${image_url}`)
+  },
 }
 
 export default MoviesAPI;
