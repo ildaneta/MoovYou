b6bebbf11c108481b3e77520cf92a27194a27675
ildaneta <iildaemanoely@gmail.com>
2022-01-04 12:54:13 -0300

    feat: inserting external fonts and showing load if the fonts are not loaded

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'react-native';
+import {
+  Roboto_300Light,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_300Light,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
     </>
   );
 }
