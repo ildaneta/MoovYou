f26d0ecbbe0c01c8c9241a936cf7c0d2293024f3
ildaneta <iildaemanoely@gmail.com>
2022-01-04 19:27:32 -0300

    feat: creating the text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text as TextComponent, StyleSheet } from 'react-native';
+import { AlignTypes } from '../../utils/enum';
 
-import { styles } from './styles';
+interface ITextComponentProps {
+  textAlign: AlignTypes;
+  label: string;
+  color: string;
+  fontSize: number;
+  fontFamily: string;
+}
 
-const Text = (): JSX.Element => {
-  return <View></View>;
+const Text = ({
+  textAlign,
+  label,
+  color,
+  fontSize,
+  fontFamily,
+}: ITextComponentProps): JSX.Element => {
+  const stylesProps = StyleSheet.create({
+    text: {
+      textAlign,
+      color,
+      fontFamily,
+      fontSize,
+    },
+  });
+
+  return <TextComponent style={stylesProps.text}>{label}</TextComponent>;
 };
 
 export default Text;
