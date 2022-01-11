093e221f87d8c8885ec22ec716c9b05c72748269
ildaneta <iildaemanoely@gmail.com>
2022-01-10 22:37:25 -0300

    feat: creating the rates component

--- src/components/Rates/index.tsx
+++ src/components/Rates/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import theme from '../../theme';
+import Text from '../Text';
+
+import StarSVG from '../../images/star-icon.svg';
 
 import { styles } from './styles';
 
-const Rates = (): JSX.Element => {
+interface IRatesProps {
+  vote: string;
+}
+
+const Rates = ({ vote }: IRatesProps): JSX.Element => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      {!!vote && (
+        <>
+          <StarSVG />
+
+          <View style={styles.divider} />
+
+          <Text
+            label={vote ? `${vote} / 10 IMDb` : ''}
+            color={theme.colors.neutral_light_gray}
+            fontFamily={theme.fonts.Medium}
+            fontSize={theme.fontsSize.Medium14}
+          />
+        </>
+      )}
     </View>
   );
 };
