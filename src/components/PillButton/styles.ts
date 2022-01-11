35e832b454f1468dd3b645c04c2b35c51164409f
ildaneta <iildaemanoely@gmail.com>
2022-01-10 22:13:40 -0300

    feat: creating the pill button component

--- src/components/PillButton/styles.ts
+++ src/components/PillButton/styles.ts
@@ -1,5 +1,15 @@
 import { StyleSheet } from 'react-native';
+import theme from '../../theme';
+import { AlignTypes } from '../../utils/enum';
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: theme.dimensions.Small6,
+    paddingVertical: theme.dimensions.Small6,
+    borderColor: theme.colors.neutral_gray,
+    borderWidth: theme.dimensions.Thin05,
+    borderRadius: theme.dimensions.Medium16,
+    justifyContent: AlignTypes.center,
+    alignItems: 'center'
+  },
 })
