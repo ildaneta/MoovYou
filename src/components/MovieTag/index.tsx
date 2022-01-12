bcfb185e59ac814433112ab931c22d8e404b4c89
ildaneta <iildaemanoely@gmail.com>
2022-01-12 19:01:03 -0300

    feat: creating the movie tag component and its styles

--- src/components/MovieTag/index.tsx
+++ src/components/MovieTag/index.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image } from 'react-native';
+import { MoviesDTO } from '../../dtos/MoviesDTO';
+import theme from '../../theme';
+import { getNameGenres } from '../../utils/getNameGenres';
+
+import Like from '../Like';
+import Rates from '../Rates';
+import Tag from '../Tag';
+import Text from '../Text';
 
 import { styles } from './styles';
 
-const MovieTag = (): JSX.Element => {
+interface IMovieTagProps {
+  data: MoviesDTO;
+  isLiked: boolean;
+}
+
+const MovieTag = ({ data, isLiked }: IMovieTagProps): JSX.Element => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
+        }}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <View style={styles.containerLike}>
+        <Like isLiked={isLiked} />
+      </View>
+
+      <View>
+        <Text
+          label={data.title}
+          color={theme.colors.neutral_gray}
+          fontFamily={theme.fonts.Bold}
+          fontSize={theme.fontsSize.Medium14}
+        />
+
+        <View style={styles.dividerTextRates} />
+
+        <Rates vote={data.vote_average} />
+        <View style={styles.containerTags}>
+          {data.genre_ids.map(genreId => (
+            <Tag
+              title={getNameGenres(genreId)}
+              tagType="PillTag"
+              key={genreId}
+            />
+          ))}
+        </View>
+      </View>
     </View>
   );
 };
--- src/components/MovieTag/styles.ts
+++ src/components/MovieTag/styles.ts
@@ -1,5 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
+import theme from '../../theme';
+import { AlignTypes } from '../../utils/enum';
+
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    flexDirection: AlignTypes.row,
+    
+  },
+
+  containerLike: {
+    position: AlignTypes.absolute,
+    left: theme.dimensions.Quarck4,
+    top: 180
+  },
+
+  image: {
+    position: AlignTypes.relative,
+    width: 130,
+    height: 200,
+    borderRadius: theme.dimensions.XXXS10,
+    marginRight: theme.dimensions.XXS12
+  },
+
+  
+  
+  dividerTextRates: {
+    marginBottom: theme.dimensions.XXXS10
+  },
+
+  containerTags: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '99%',
+    paddingTop: 90,
+  }
 })
