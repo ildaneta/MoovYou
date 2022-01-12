5fb9b280d1b90dfce0f7bc891ecd36bb79e45d69
ildaneta <iildaemanoely@gmail.com>
2022-01-12 08:51:03 -0300

    feat: creating the movie rate component

--- src/components/MovieRate/index.tsx
+++ src/components/MovieRate/index.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Image,
+  TouchableOpacityProps,
+  TouchableOpacity,
+} from 'react-native';
+
+import SpidermanImg from '../../images/spiderman.jpeg';
+import theme from '../../theme';
+import Like from '../Like';
+import Rates from '../Rates';
+import Text from '../Text';
 
 import { styles } from './styles';
 
-const MovieRate = (): JSX.Element => {
+interface IMovieRateProps extends TouchableOpacityProps {
+  isLiked: boolean;
+}
+
+const MovieRate = ({ isLiked, ...rest }: IMovieRateProps): JSX.Element => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity {...rest} activeOpacity={0.9}>
+      <View style={styles.containerImage}>
+        <Image
+          source={require('../../images/homem-aranha.png')}
+          resizeMode="cover"
+          style={styles.image}
+        />
+        <View style={styles.containerLike}>
+          <Like isLiked={isLiked} />
+        </View>
+      </View>
+
+      <View style={styles.containerText}>
+        <Text
+          label="Spiderman: No Way Home"
+          fontFamily={theme.fonts.Bold}
+          color={theme.colors.neutral_gray}
+          fontSize={theme.fontsSize.Medium14}
+        />
+      </View>
+
+      <Rates vote={'8.5'} />
+    </TouchableOpacity>
   );
 };
 
