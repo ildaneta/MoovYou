4a05054383a8c639ad5ac60e5f6fdd890ac898a2
ildaneta <iildaemanoely@gmail.com>
2022-01-12 12:07:09 -0300

    refactor: excluding the api image

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,4 @@ const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/"
 });
 
-const apiImage = axios.create({
-  baseURL: "https://image.tmdb.org/t/p/w500/"
-})
-
-export {api, apiImage};
+export default api
