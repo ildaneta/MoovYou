91891421addb072a2cb38581e1fdb73b7288e47d
ildaneta <iildaemanoely@gmail.com>
2022-01-12 13:51:27 -0300

    feat: creating the tag component and its styles

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import theme from '../../theme';
+import Text from '../Text';
 
 import { styles } from './styles';
 
-const Tag = (): JSX.Element => {
+interface ITagProps {
+  tagType: 'PillTag' | 'SquareTag';
+  title: string;
+}
+
+const Tag = ({ tagType, title }: ITagProps): JSX.Element => {
+  const styleProps = StyleSheet.create({
+    tagContainer: {
+      paddingVertical:
+        tagType === 'PillTag'
+          ? theme.dimensions.Small6
+          : theme.dimensions.Quarck4,
+      borderRadius:
+        tagType === 'PillTag'
+          ? theme.dimensions.Medium16
+          : theme.dimensions.Quarck4,
+    },
+  });
+
   return (
-    <View>
-      <Text></Text>
+    <View style={[styles.container, styleProps.tagContainer]}>
+      <Text
+        label={title}
+        color={theme.colors.neutral_gray}
+        fontFamily={theme.fonts.Light}
+        fontSize={theme.fontsSize.XS12}
+        isUpperCase
+      />
     </View>
   );
 };
