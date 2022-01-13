deb544e84c276ca3b4ef6ac75d31f867fecb4bbf
ildaneta <iildaemanoely@gmail.com>
2022-01-12 21:18:18 -0300

    feat: creating the search page sctructure

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+const Search = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Search page</Text>
+    </View>
+  );
+};
+
+export default Search;
