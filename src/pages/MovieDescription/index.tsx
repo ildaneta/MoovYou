a5b7fc865ec02f871a08fbb921f180e17ce968b0
ildaneta <iildaemanoely@gmail.com>
2022-01-13 21:43:42 -0300

    feat: bringing the props from Home page

--- src/pages/MovieDescription/index.tsx
+++ src/pages/MovieDescription/index.tsx
@@ -1,12 +1,26 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { IStackRoutes } from '../../routes/stack.routes';
 
 import { styles } from './styles';
 
-const MovieDescription = (): JSX.Element => {
+type Props = {
+  route: {
+    params: {
+      movieId: number;
+    };
+  };
+};
+
+const MovieDescription = ({ route }: Props): JSX.Element => {
+  const { movieId } = route.params;
+
+  console.log('movie ID', movieId);
+
   return (
     <View>
-      <Text></Text>
+      <Text>Movie Description</Text>
     </View>
   );
 };
