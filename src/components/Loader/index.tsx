6a8eb03820f158529406c0f24d299c4b50d072c4
ildaneta <iildaemanoely@gmail.com>
2022-01-17 20:07:02 -0300

    refactor: adding a style to activity indicator

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { ActivityIndicator } from 'react-native';
 import theme from '../../theme';
 
+import { styles } from './styles';
 interface ILoaderProps {
   size: 'small' | 'large';
 }
 
 const Loader = ({ size }: ILoaderProps): JSX.Element => {
-  return <ActivityIndicator size={size} color={theme.colors.neutral_white} />;
+  return (
+    <ActivityIndicator
+      size={size}
+      color={theme.colors.neutral_white}
+      style={styles.activityIndicator}
+    />
+  );
 };
 
 export default Loader;
