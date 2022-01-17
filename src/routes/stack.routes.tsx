2aa96ca6fb7b72eb777c62c83c440b64181e1e3b
ildaneta <iildaemanoely@gmail.com>
2022-01-17 16:03:25 -0300

    refactor: adding a walkthrough route and component

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import { RoutesName } from '../utils/routesName';
 import Home from '../pages/Home';
 import MovieDescription from '../pages/MovieDescription';
 import TabRoutes from './tab.routes';
+import Walkthrough from '../pages/Walkthrough';
 
 export type IStackRoutes = {
   Home: undefined;
@@ -12,6 +13,7 @@ export type IStackRoutes = {
     movieId: number;
   };
   Tab_Routes: undefined;
+  Walkthrough: undefined;
 };
 
 const { Navigator, Screen } = createNativeStackNavigator<IStackRoutes>();
@@ -20,6 +22,7 @@ const StackRoutes = (): JSX.Element => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name={RoutesName.TAB_ROUTES} component={TabRoutes} />
+      <Screen name={RoutesName.WALKTHROUGH} component={Walkthrough} />
       <Screen name={RoutesName.HOME} component={Home} />
       <Screen
         name={RoutesName.MOVIE_DESCRIPTION}
