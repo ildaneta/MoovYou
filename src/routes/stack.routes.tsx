d657a1d7f8de94a41b094a6192e7d85d5ef6afda
ildaneta <iildaemanoely@gmail.com>
2022-01-17 16:17:30 -0300

    refactor: editing the routes order

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -22,12 +22,12 @@ const StackRoutes = (): JSX.Element => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name={RoutesName.TAB_ROUTES} component={TabRoutes} />
-      <Screen name={RoutesName.WALKTHROUGH} component={Walkthrough} />
       <Screen name={RoutesName.HOME} component={Home} />
       <Screen
         name={RoutesName.MOVIE_DESCRIPTION}
         component={MovieDescription}
       />
+      <Screen name={RoutesName.WALKTHROUGH} component={Walkthrough} />
     </Navigator>
   );
 };
