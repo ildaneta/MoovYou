91976f609c808f607caa85c624bc1f8a87af0a56
ildaneta <iildaemanoely@gmail.com>
2022-01-17 20:05:42 -0300

    feat: creating the search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,13 +1,126 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { View, TextInput, TouchableOpacity } from 'react-native';
+import theme from '../../theme';
+
+import SearchIconSVG from '../../images/search-icon-page.svg';
 
 import { styles } from './styles';
+import MoviesAPI from '../../services/movies-api';
+import MovieRate from '../../components/MovieRate';
+import { FlatList } from 'react-native-gesture-handler';
+import { RoutesName } from '../../utils/routesName';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { IStackRoutes } from '../../routes/stack.routes';
 
-const Search = (): JSX.Element => {
+import { IMoviesProps } from '../Home';
+import Header from '../../components/Header';
+import { useFocusEffect } from '@react-navigation/native';
+import Loader from '../../components/Loader';
+
+type SearchScreenNavigationProp = NativeStackNavigationProp<
+  IStackRoutes,
+  'Search'
+>;
+
+type Props = {
+  navigation: SearchScreenNavigationProp;
+};
+
+const Search = ({ navigation }: Props): JSX.Element => {
+  const [page, setPage] = useState(1);
+  const [inputSearchText, setInputSearchText] = useState('');
+  const [movieSearched, setMovieSearched] = useState<IMoviesProps>(
+    {} as IMoviesProps,
+  );
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearchMovie = (text: string) => {
+    if (!text) {
+      console.log('a busca nao pode estar vazia');
+    }
+    setInputSearchText(text);
+  };
+
+  const getMovieSearched = async () => {
+    try {
+      setIsLoading(true);
+      const response = await MoviesAPI.getMoviesSearch(
+        page,
+        inputSearchText && inputSearchText,
+      );
+      setMovieSearched(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const selectedMovie = (id: number) => {
+    const movieIdSelected = movieSearched!.results.find(
+      movie => movie.id === id,
+    );
+
+    navigation.navigate(RoutesName.MOVIE_DESCRIPTION, {
+      movieId: movieIdSelected?.id!,
+    });
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      setInputSearchText('');
+      setMovieSearched({});
+    }, []),
+  );
   return (
-    <View>
-      <Text>Search page</Text>
-    </View>
+    <>
+      <View style={styles.header}>
+        <Header />
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.containerSearch}>
+          <TextInput
+            onChangeText={handleSearchMovie}
+            onSubmitEditing={getMovieSearched}
+            placeholder="Search your movie"
+            style={styles.input}
+            placeholderTextColor={theme.colors.neutral_light_gray}
+            value={inputSearchText}
+          />
+
+          <View style={styles.search}>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => {
+                getMovieSearched();
+              }}>
+              <SearchIconSVG />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {isLoading ? (
+          <Loader size="small" />
+        ) : (
+          <View style={styles.containerMovie}>
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={movieSearched.results}
+              keyExtractor={item => String(item.id)}
+              numColumns={2}
+              renderItem={({ item }) => (
+                <MovieRate
+                  data={item}
+                  isLiked={false}
+                  onPress={() => selectedMovie(item.id)}
+                  isSearch
+                />
+              )}
+            />
+          </View>
+        )}
+      </View>
+    </>
   );
 };
 
--- src/pages/Search/styles.ts
+++ src/pages/Search/styles.ts
@@ -1,5 +1,49 @@
 import { StyleSheet } from 'react-native';
+import theme from '../../theme';
+import { AlignTypes } from '../../utils/enum';
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: theme.dimensions.Thin1,
+    backgroundColor: theme.colors.neutral_black,
+    paddingHorizontal: theme.dimensions.Medium16
+  },
+
+  header:{
+    backgroundColor: theme.colors.neutral_black,
+    paddingHorizontal: theme.dimensions.Medium16
+  },
+
+  input: {
+    borderColor: theme.colors.neutral_white,
+    borderRightColor: theme.colors.neutral_gray,
+    borderWidth: theme.dimensions.Thin1,
+    paddingVertical: theme.dimensions.Small8,
+    paddingHorizontal: theme.dimensions.Medium16,
+    borderTopLeftRadius: theme.dimensions.Small6,
+    borderBottomLeftRadius: theme.dimensions.Small6,
+    width: theme.dimensions.width90p,
+    color: theme.colors.neutral_white
+  },
+
+  containerSearch: {
+    flexDirection: AlignTypes.row,
+    alignItems: AlignTypes.center
+  },
+
+  search: {
+    width: theme.dimensions.width10p,
+    alignItems: AlignTypes.center,
+    borderColor: theme.colors.neutral_white,
+    borderTopWidth: theme.dimensions.Thin1,
+    borderBottomWidth: theme.dimensions.Thin1,
+    borderRightWidth: theme.dimensions.Thin1,
+    paddingVertical: theme.dimensions.Small7,
+    borderTopRightRadius: theme.dimensions.Small6,
+    borderBottomRightRadius: theme.dimensions.Small6,
+  },
+
+  containerMovie: {
+    paddingTop: theme.dimensions.Large20,
+  }
 })
