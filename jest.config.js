b8d4767c2bfb7ab2ddf2de187e747a9dcee6c0b5
ildaneta <iildaemanoely@gmail.com>
2022-01-21 09:01:19 -0300

    tests: adding the jest configs

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'jest-expo',
+  testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+};
