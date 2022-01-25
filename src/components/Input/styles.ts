9680fe466e3cf838243e20eff93effa224f77af7
ildaneta <iildaemanoely@gmail.com>
2022-01-25 09:39:41 -0300

    refactor: changing the input component adding the button search and the error text

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,7 +1,16 @@
 import { StyleSheet } from 'react-native';
 import theme from '../../theme';
+import { AlignTypes } from '../../utils/enum';
 
 export const styles = StyleSheet.create({
+  containerInput: {
+    flexDirection: AlignTypes.row,
+  },
+
+  textError: {
+    paddingTop: theme.dimensions.Small5
+  },
+
   input: {
     borderColor: theme.colors.neutral_white,
     borderRightColor: theme.colors.neutral_gray,
@@ -14,4 +23,22 @@ export const styles = StyleSheet.create({
     color: theme.colors.neutral_white,
     height: theme.dimensions.Hudge40
   },
+
+  search: {
+    width: theme.dimensions.width10p,
+    alignItems: AlignTypes.center,
+    justifyContent: AlignTypes.center,
+    borderColor: theme.colors.neutral_white,
+    borderTopWidth: theme.dimensions.Thin1,
+    borderBottomWidth: theme.dimensions.Thin1,
+    borderRightWidth: theme.dimensions.Thin1,
+    paddingVertical: theme.dimensions.Small7,
+    borderTopRightRadius: theme.dimensions.Small6,
+    borderBottomRightRadius: theme.dimensions.Small6,
+    height: theme.dimensions.Hudge40
+  },
+
+  inputError: {
+    borderColor: theme.colors.neutral_red
+  }
 })
