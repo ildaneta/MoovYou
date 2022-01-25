532fc22077a70bc5ec41108db84740e4966b7398
ildaneta <iildaemanoely@gmail.com>
2022-01-24 21:37:15 -0300

    refactor: checking if the user is logged

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackRoutes from './stack.routes';
 
+import { useAuthContext } from '../context/AuthContext';
+import Login from '../pages/Login';
+
 const Routes = (): JSX.Element => {
+  const { user } = useAuthContext();
+
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {user.id ? <StackRoutes /> : <Login />}
     </NavigationContainer>
   );
 };
