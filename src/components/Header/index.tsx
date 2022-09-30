38639023b40a9a839850ef9a79daef2903bc9e9f
ildaneta <iildaemanoely@gmail.com>
2022-09-30 07:27:50 -0300

    refactor: change moov you icon brand

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 
 import MoovYouBrandSVG from '../../images/moovyou-brand-icon.svg';
+import IconBrandSVG from '../../images/icon-brand-slider.svg';
 
 import { styles } from './styles';
 
 const Header = (): JSX.Element => {
   return (
     <View style={styles.brandIcon}>
-      <MoovYouBrandSVG />
+      <IconBrandSVG />
     </View>
   );
 };
